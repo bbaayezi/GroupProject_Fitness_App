b722764759f40015de7ed8d1f00a01d6e8f22bef
cai <y.cai26@student.liverpool.ac.uk>
2019-04-15 01:06:56 +0800

    update recipe

--- src/store.js
+++ src/store.js
@@ -103,8 +103,6 @@ export default new Vuex.Store({
           fat: 6.0,
           carbs: 24,
           protein: 4.0,
-          sodium: 87,
-          calcium: '14%',
           iron: '1%'
         },
         {
@@ -113,8 +111,6 @@ export default new Vuex.Store({
           fat: 9.0,
           carbs: 37,
           protein: 4.3,
-          sodium: 129,
-          calcium: '8%',
           iron: '1%'
         },
         {
@@ -123,8 +119,6 @@ export default new Vuex.Store({
           fat: 16.0,
           carbs: 23,
           protein: 6.0,
-          sodium: 337,
-          calcium: '6%',
           iron: '7%'
         },
         {
@@ -133,8 +127,6 @@ export default new Vuex.Store({
           fat: 3.7,
           carbs: 67,
           protein: 4.3,
-          sodium: 413,
-          calcium: '3%',
           iron: '8%'
         },
         {
@@ -143,8 +135,6 @@ export default new Vuex.Store({
           fat: 16.0,
           carbs: 49,
           protein: 3.9,
-          sodium: 327,
-          calcium: '7%',
           iron: '16%'
         },
         {
@@ -153,8 +143,6 @@ export default new Vuex.Store({
           fat: 0.0,
           carbs: 94,
           protein: 0.0,
-          sodium: 50,
-          calcium: '0%',
           iron: '0%'
         },
         {
@@ -163,8 +151,6 @@ export default new Vuex.Store({
           fat: 0.2,
           carbs: 98,
           protein: 0,
-          sodium: 38,
-          calcium: '0%',
           iron: '2%'
         },
         {
@@ -173,8 +159,6 @@ export default new Vuex.Store({
           fat: 3.2,
           carbs: 87,
           protein: 6.5,
-          sodium: 562,
-          calcium: '0%',
           iron: '45%'
         },
         {
@@ -183,8 +167,6 @@ export default new Vuex.Store({
           fat: 25.0,
           carbs: 51,
           protein: 4.9,
-          sodium: 326,
-          calcium: '2%',
           iron: '22%'
         },
         {
@@ -193,10 +175,9 @@ export default new Vuex.Store({
           fat: 26.0,
           carbs: 65,
           protein: 7,
-          sodium: 54,
-          calcium: '12%',
           iron: '6%'
         }
+
       ],
     },
     aboutPage:{
--- src/views/Recipe.vue
+++ src/views/Recipe.vue
@@ -1,29 +1,56 @@
 <template>
   <v-content>
-    <div class="display-3 font-weight-bold orange--text text-md-center">Recipe Recommendation</div><br>
-    <v-layout>
-     <DataIterator/>
+    <div class="display-3 font-weight-bold orange--text text-md-center">Recipe Recommendation</div>
+    <br>
+    <v-layout row justify-center>
+        <v-flex xs2>
+      <div class="display-1 font-weight-bold white--text light-green darken-1 text-md-center">Lose Weight<br></div>
+    </v-flex>
     </v-layout>
+    <br>
+    <v-layout justify-space-around row wrap>
+      <DataTable/>
+    </v-layout>
+    <br>
+    <br>
+    <v-layout row justify-center>
+        <v-flex xs2>
+    <div class="display-1 font-weight-bold white--text light-green darken-1 text-md-center">Get Strong</div>
+    </v-flex>
+    </v-layout>
+     <br>
+    <v-layout justify-space-around row wrap>
+      <DataTable/>
+    </v-layout>
+    <br>
+    <br>
+     <v-layout row justify-center>
+        <v-flex xs2>
+    <div class="display-1 font-weight-bold white--text light-green darken-1 text-md-center">Get Fit</div>
+    </v-flex>
+    </v-layout>
+    <br>
+    <v-layout justify-space-around row wrap>
+      <DataTable/>
+    </v-layout>
+    <br>
+    <br>
   </v-content>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
-import DataIterator from "../components/DataIterator";
+import DataTable from "../components/DataTable";
 
 export default {
   data() {
-    return {
-      
-    };
+    return {};
   },
   components: {
-    DataIterator,
+    DataTable
   },
   computed: {
-    ...mapGetters([
-      "getNutritionList",
-    ])
+    ...mapGetters(["getNutritionList"])
   }
 };
 </script>
