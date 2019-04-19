804dda202d78bd5bf9cb2bca6084380c80173dd2
Yucheng <yucheng.peng97@outlook.com>
2019-04-19 18:22:28 +0100

    recipe 更改互动 18:22 4/19

--- src/views/Recipe.vue
+++ src/views/Recipe.vue
@@ -3,11 +3,26 @@
     <v-container grid-list-md fluid>
       <v-layout row wrap>
         <v-flex xs12 md6>
-          <v-card>
+          <v-hover>
+          <v-card
+          slot-scope="{ hover }"
+      :class="`elevation-${hover ? 12 : 2}`"
+      class="mx-auto"
+      >
             <v-img
               src="https://cdn-image.foodandwine.com/sites/default/files/styles/medium_2x/public/buying-healthy-foods-ft-blog0617.jpg?itok=fdrZyzB6"
               :aspect-ratio="isMobile ? 1.7 : 0.64"
-            ></v-img>
+            >
+            <v-expand-transition>
+          <div
+            v-if="hover"
+            class="d-flex transition-fast-in-fast-out green darken-2 v-card--reveal display-3 white--text"
+            style="height: 100%;"
+          >
+            🍆
+          </div>
+        </v-expand-transition>
+        </v-img>
             <v-card-title primary-title>
               <div class="display-2">Vegetables</div>
               <v-spacer></v-spacer>
@@ -16,15 +31,29 @@
               </v-card-actions>
             </v-card-title>
           </v-card>
+          </v-hover>
         </v-flex>
         <v-flex xs12 md6>
           <v-layout row wrap>
             <v-flex xs12>
-              <v-card>
+              <v-hover>
+              <v-card
+              slot-scope="{ hover }"
+      :class="`elevation-${hover ? 12 : 2}`"
+      class="mx-auto">
                 <v-img
                   src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/super-food-for-healthy-eating-royalty-free-image-928553584-1549508731.jpg"
                   :aspect-ratio="isMobile ? 1.7 : 1.5"
                 ></v-img>
+                <v-expand-transition>
+          <div
+            v-if="hover"
+            class="d-flex transition-fast-in-fast-out yellow darken-2 v-card--reveal display-3 white--text"
+            style="height: 100%;"
+          >
+            🍉
+          </div>
+        </v-expand-transition>
                 <v-card-title primary-title>
                   <div class="display-2">Fruits</div>
                   <v-spacer></v-spacer>
@@ -33,27 +62,55 @@
                   </v-card-actions>
                 </v-card-title>
               </v-card>
+              </v-hover>
             </v-flex>
             <v-flex xs12>
-              <v-card>
+              <v-hover>
+              <v-card
+              slot-scope="{ hover }"
+      :class="`elevation-${hover ? 12 : 2}`"
+      class="mx-auto">
                 <v-img
                   src="https://stateofmind13.files.wordpress.com/2012/04/raw-meat.jpg"
                   :aspect-ratio="isMobile ? 1.7 : 1.5"
                 ></v-img>
+                 <v-expand-transition>
+          <div
+            v-if="hover"
+            class="d-flex transition-fast-in-fast-out red darken-2 v-card--reveal display-3 white--text"
+            style="height: 100%;"
+          >
+            🍖
+          </div>
+        </v-expand-transition>
                 <v-card-title primary-title>
                   <div class="display-2">Meats</div>
                   <v-spacer></v-spacer>
                   <v-card-actions>
-                    <v-btn color="orange" flat>DETAIL</v-btn>
+                    <v-btn color="blue" flat>DETAIL</v-btn>
                   </v-card-actions>
                 </v-card-title>
               </v-card>
+              </v-hover>
             </v-flex>
           </v-layout>
         </v-flex>
         <v-flex xs12>
-          <v-card>
+          <v-hover>
+          <v-card
+          slot-scope="{ hover }"
+      :class="`elevation-${hover ? 12 : 2}`"
+      class="mx-auto">
             <v-img src="https://www.rd.com/wp-content/uploads/2018/06/healthy-food.jpg"></v-img>
+             <v-expand-transition>
+          <div
+            v-if="hover"
+            class="d-flex transition-fast-in-fast-out black darken-2 v-card--reveal display-3 white--text"
+            style="height: 100%;"
+          >
+            🍽
+          </div>
+        </v-expand-transition>
             <v-card-title primary-title>
               <div class="display-2">Diet</div>
               <v-spacer></v-spacer>
@@ -62,6 +119,7 @@
               </v-card-actions>
             </v-card-title>
           </v-card>
+          </v-hover>
         </v-flex>
 
         <v-flex xs12 mt-3>
@@ -103,3 +161,14 @@ export default {
   }
 };
 </script>
+
+<style>
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: .5;
+  position: absolute;
+  width: 100%;
+}
+</style>
