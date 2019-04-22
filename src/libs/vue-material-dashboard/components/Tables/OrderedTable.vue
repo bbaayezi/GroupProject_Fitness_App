b7b1493d95ae76c7dfe5f14a60cb33ae0e9f4616
hjy <378441765@qq.com>
2019-04-22 21:41:09 +0800

    update order table data

--- src/libs/vue-material-dashboard/components/Tables/OrderedTable.vue
+++ src/libs/vue-material-dashboard/components/Tables/OrderedTable.vue
@@ -29,28 +29,28 @@ export default {
           name: "Broccoli",
           calories: "55 kcal",
           carbohydrate: "3.1 g",
-          fiber: "2.7 g",
+          fibre: "2.7 g",
           vitamin: "Vitamin K1: 280% RDA Vitamin A: 52% RDA Vitamin C: 34% RDA"
         },
         {
           name: "Carrots",
           calories: "41 kcal",
           carbohydrate: "9.6 g",
-          fiber: "2.8 g",
+          fibre: "2.8 g",
           vitamin: "Vitamin A: 334% RDA Vitamin K1: 16% RDA Vitamin C: 10% RDA Potassium: 9% RDA"
         },
         {
           name: "Bok Choy",
           calories: "13 kcal",
           carbohydrate: "2.2 g",
-          fiber: "1.0 g",
+          fibre: "1.0 g",
           vitamin: "Vitamin A: 89% RDA Vitamin C: 75% RDA Vitamin K: 57% RDA"
         },
         {
           name: "Beets",
           calories: "43 kcal",
           carbohydrate: "9.6 g",
-          fiber: "2.8 g",
+          fibre: "2.8 g",
           vitamin: "Vitamin C: 8% RDA"
         },
         {
@@ -61,13 +61,6 @@ export default {
           vitamin: "Vitamin C: 306% RDA Vitamin B6: 8% RDA"
         },
           {
-          name: "Pork",
-          calories: "297 kcal / 100 g",
-          fat: "20.8 grams / 100 g",
-          protein: "25.7 grams",
-          vitamin: "Vitamin C: 306% RDA Vitamin B6: 8% RDA",
-          water: "53%"
-        },{
           name: "Strawberry",
           calories: "39 cal / 100 g",
           moisture_content: "89 grams / 100 g",
@@ -77,26 +70,34 @@ export default {
           name: "Banana",
           calories: "91 cal / 100 g",
           moisture_content: "74 grams / 100 g",
-          cellulose: "1.6 grams",
+          fibre: "1.6 grams",
           vitamin: "Vitamin A: 8 IU Vitamin C: 10.1 mg"
         },{
           name: "Pineapple",
           calories: "46 cal / 100 g",
           moisture_content: "87 grams / 100 g",
-          cellulose: "1.4 grams",
+          fibre: "1.4 grams",
           vitamin: "Vitamin A: 17 IU Vitamin C: 9.0 mg"
         },{
           name: "Apple",
           calories: "50 cal / 100 g",
           moisture_content: "86 grams / 100 g",
-          cellulose: "1.6 grams",
+          fibre: "1.6 grams",
           vitamin: "Vitamin A: 13 IU Vitamin C: 2.1 mg"
         },{
           name: "Pawpaw",
           calories: "52 cal / 100 g",
           moisture_content: "85 grams / 100 g",
-          cellulose: "1.7 grams",
+          fibre: "1.7 grams",
           vitamin: "Vitamin A: 134 IU Vitamin C: 74.0 mg"
+        },
+        {
+          name: "Pork",
+          calories: "297 kcal / 100 g",
+          fat: "20.8 grams / 100 g",
+          protein: "25.7 grams",
+          vitamin: "Vitamin C: 306% RDA Vitamin B6: 8% RDA",
+          water: "53%"
         },
         {
           name: "Beef",
@@ -127,49 +128,49 @@ export default {
           water: "40%"
         },
         {
-          Name: "Minty roast veg & hummus salad",
-          Calories: "611 kcal",
-          Fat: "26 g",
-          Carbohydrate: "61 g",
-          Fibre: "20 g",
-          Protein: "23 g",
-          Salt: "3.5g"
+          name: "Minty roast veg & hummus salad",
+          calories: "611 kcal",
+          fat: "26 g",
+          carbohydrate: "61 g",
+          fibre: "20 g",
+          protein: "23 g",
+          salt: "3.5g"
         },
         {
-          Name: "Spanish meatball & butter bean stew",
-          Calories: "435 kcal",
-          Fat: "15 g",
-          Carbohydrate: "35 g",
-          Fibre: "12 g",
-          Protein: "33 g",
-          Salt: "1.3 g"
+          name: "Spanish meatball & butter bean stew",
+          calories: "435 kcal",
+          fat: "15 g",
+          carbohydrate: "35 g",
+          fibre: "12 g",
+          protein: "33 g",
+          salt: "1.3 g"
         },
         {
-          Name: "Smoky hake, beans & greens",
-          Calories: "554 kcal",
-          Fat: "27 g",
-          Carbohydrate: "28 g",
-          Fibre: "6 g",
-          Protein: "45 g",
-          Salt: "2.2 g"
+          name: "Smoky hake, beans & greens",
+          calories: "554 kcal",
+          fat: "27 g",
+          carbohydrate: "28 g",
+          fibre: "6 g",
+          protein: "45 g",
+          salt: "2.2 g"
         },
         {
-          Name: "Spicy roast veg & lentils",
-          Calories: "321 kcal",
-          Fat: "12 g",
-          Carbohydrate: "38 g",
-          Fibre: "11 g",
-          Protein: "10 g",
-          Salt: "0.6 g"
+          name: "Spicy roast veg & lentils",
+          calories: "321 kcal",
+          fat: "12 g",
+          carbohydrate: "38 g",
+          fibre: "11 g",
+          protein: "10 g",
+          salt: "0.6 g"
         },
         {
-          Name: "Mushroom fajitas with avocado hummus",
-          Calories: "824 kcal",
-          Fat: "36 g",
-          Carbohydrate: "104 g",
-          Fibre: "14 g",
-          Protein: "23 g",
-          Salt: "2.3 g"
+          name: "Mushroom fajitas with avocado hummus",
+          calories: "824 kcal",
+          fat: "36 g",
+          carbohydrate: "104 g",
+          fibre: "14 g",
+          protein: "23 g",
+          salt: "2.3 g"
         },
       ]
     };
