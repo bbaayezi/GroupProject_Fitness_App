211ec14bb2c707c8ab2fdd32e67357a3b1308ab0
bbaayezi <bbaayezi@gmail.com>
2019-04-24 15:56:55 +0100

    update record table and link animate.css

--- src/components/RecordTable.vue
+++ src/components/RecordTable.vue
@@ -1,46 +1,115 @@
 <template>
-  <v-layout row>
-    <v-flex xs11 sm10 md10>
-      <v-card>
+  <md-card>
+    <md-card-header data-background-color="green">
+      <div class="display-1">Task Table</div>
+    </md-card-header>
+    <v-list two-line>
+      <transition-group enter-active-class="animated slideInLeft" leave-active-class="animated fadeOutUp">
+        <v-list-tile v-for="(item, id) in tasks" :key="id">
+          <span class="mr-2">
+            <v-icon large>event_note</v-icon>
+          </span>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              <input type="text" v-model="item.title" :readonly="item.readonly">
+            </v-list-tile-title>
+            <v-list-tile-sub-title>
+              <input type="text" v-model="item.content" :readonly="item.readonly">
+            </v-list-tile-sub-title>
+          </v-list-tile-content>
+          <v-list-tile-action>
+            <v-tooltip top>
+              <template v-slot:activator="{ on }">
+                <v-btn icon v-on="on" @click="itemClick(item)">
+                  <v-icon large>{{ item.readonly ? "create" : "done_all" }}</v-icon>
+                </v-btn>
+              </template>
+              <span>{{ item.readonly ? "Edit" : "Finish Editing" }}</span>
+            </v-tooltip>
+          </v-list-tile-action>
+          <v-list-tile-action>
+            <v-tooltip top>
+              <template v-slot:activator="{ on }">
+                <v-btn icon v-on="on">
+                  <v-icon large>check</v-icon>
+                </v-btn>
+              </template>
+              <span>Mark as finished</span>
+            </v-tooltip>
+          </v-list-tile-action>
+          <v-list-tile-action>
+            <v-tooltip top>
+              <template v-slot:activator="{ on }">
+                <v-btn icon v-on="on" @click="removeTask(item)">
+                  <v-icon large>clear</v-icon>
+                </v-btn>
+              </template>
+              <span>Delete Task</span>
+            </v-tooltip>
+          </v-list-tile-action>
+        </v-list-tile>
+      </transition-group>
 
-        <v-list two-line subheader>
-
-          <v-list-tile
-            v-for="item in items"
-            :key="item.title"
-            avatar
-          >
-            <v-list-tile-avatar>
-              <v-icon :class="[item.iconClass]">{{ item.icon }}</v-icon>
-            </v-list-tile-avatar>
-
-            <v-list-tile-content>
-              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-              <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
-            </v-list-tile-content>
-
-            <v-list-tile-action>
-              <v-btn icon ripple>
-                <v-icon color="grey lighten-1">info</v-icon>
+      <v-layout justify-center>
+        <v-list-tile>
+          <v-tooltip top>
+            <template v-slot:activator="{ on }">
+              <v-btn icon v-on="on" @click="createTask()">
+                <v-icon large>add_circle</v-icon>
               </v-btn>
-            </v-list-tile-action>
-          </v-list-tile>
-        </v-list>
-      </v-card>
-    </v-flex>
-  </v-layout>
+            </template>
+            <span>Create a new task</span>
+          </v-tooltip>
+        </v-list-tile>
+      </v-layout>
+    </v-list>
+  </md-card>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        items: [
-          { icon: 'assignment', iconClass: 'blue white--text', title: 'Vacation itinerary', subtitle: 'April 10, 2019' },
-          { icon: 'call_to_action', iconClass: 'amber white--text', title: 'Kitchen remodel', subtitle: 'April 12, 2019' }
-        ]
-      }
+export default {
+  name: "nav-tabs-table",
+  data() {
+    return {
+      tasks: [
+        // {
+        //   title: "Task Title",
+        //   content: "Task Description",
+        //   finished: false,
+        //   readonly: true
+        // },
+        // {
+        //   title: "Task Title 2",
+        //   content: "Task Description 2",
+        //   finished: false,
+        //   readonly: true
+        // }
+      ],
+      taskId: 0
+    };
+  },
+  methods: {
+    itemClick(item) {
+      item.readonly = !item.readonly;
+    },
+    createTask() {
+      this.tasks.push({
+        title: `Task Title ${this.taskId}`,
+        content: "Task Description",
+        finished: false,
+        readonly: true
+      });
+      this.taskId++;
+    },
+    removeTask(item) {
+      this.tasks.splice(this.tasks.indexOf(item), 1);
     }
   }
+};
 </script>
 
+<style>
+  .animated {
+    animation-duration: 0.2s;
+  }
+</style>
--- src/libs/vue-material-dashboard/components/Tables/NavTabsTable.vue
+++ src/libs/vue-material-dashboard/components/Tables/NavTabsTable.vue
@@ -1,41 +1,21 @@
 <template>
   <div>
     <md-table v-model="users" @md-selected="onSelect">
-      <md-table-row slot="md-table-row" slot-scope="{ item }">
-        <transition name="slide-fade">
-          <md-table-cell v-if="show==0">{{ item.init }}</md-table-cell>
-        </transition>
-         <transition name="slide-fade">
-          <md-table-cell v-if="show==2">{{ item.target }}</md-table-cell>
-        </transition>
-        <transition name="slide-fade">
-          <md-table-cell v-if="show==3" class = "grey--text">{{ item.end }}</md-table-cell>
-        </transition>
-          <md-table-cell >
-            <transition name="slide-fade" >
-            <v-form v-if="show==1">
-              <v-container>
-                <v-layout row wrap>
-                  <v-flex xs12 sm6>
-                    <v-text-field input id= 'target' name = 'target' label="Your target"></v-text-field>
-                  </v-flex>
-                  <v-flex xs12 sm6>
-                    <v-btn flat class = "grey--text" @click="submit">submit</v-btn>
-                  </v-flex>
-                </v-layout>
-              </v-container>
-            </v-form>
-           </transition>
-          </md-table-cell>
-       
+      <md-table-row
+        slot="md-table-row"
+        slot-scope="{ item }"
+        md-selectable="multiple"
+        md-auto-select
+      >
+        <md-table-cell>{{ item.name }}</md-table-cell>
         <md-table-cell>
-          <md-button class="md-just-icon md-simple md-danger" v-if="show==0" v-on:click="show = 1">
+          <md-button class="md-just-icon md-simple md-primary">
             <md-icon>edit</md-icon>
             <md-tooltip md-direction="top">Edit</md-tooltip>
           </md-button>
-          <md-button class="md-just-icon md-simple md-primary" v-if="show==2" v-on:click="record()">
-            <md-icon>done</md-icon>
-            <md-tooltip md-direction="top">Done</md-tooltip>
+          <md-button class="md-just-icon md-simple md-danger">
+            <md-icon>close</md-icon>
+            <md-tooltip md-direction="top">Close</md-tooltip>
           </md-button>
         </md-table-cell>
       </md-table-row>
@@ -49,13 +29,16 @@ export default {
   data() {
     return {
       selected: [],
-      show: 0,
-      currentdate: "2019-4-23",
       users: [
         {
-          init: "Please add your target",
-          target: "null",
-          end: "You have finish your target today, come here tomorrow!"
+          name: 'Sign contract for "What are conference organizers afraid of?"'
+        },
+        {
+          name: "Lines From Great Russian Literature? Or E-mails From My Boss?"
+        },
+        {
+          name:
+            "Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit"
         }
       ]
     };
@@ -63,43 +46,7 @@ export default {
   methods: {
     onSelect: function(items) {
       this.selected = items;
-    },
-    submit(){
-      this.show = 2;
-      this.users[0].target = document.getElementById("target").value
-    },
-    computeCurrentDate() {
-      var date = new Date();
-      var seperator1 = "-";
-      var year = date.getFullYear();
-      var month = date.getMonth() + 1;
-      var strDate = date.getDate();
-      if (month >= 1 && month <= 9) {
-        month = "0" + month;
-      }
-      if (strDate >= 0 && strDate <= 9) {
-        strDate = "0" + strDate;
-      }
-      var currentdate = year + seperator1 + month + seperator1 + strDate;
-      return currentdate;
-    },
-    record(){
-      this.show = 3;
-      this.currentDate = computeCurrentDate();
     }
   }
 };
 </script>
-<style>
-.slide-fade-enter-active {
-  transition: all 0.5s ease;
-}
-.slide-fade-leave-active {
-  transition: all 0.5s cubic-bezier(1, 0.5, 0.8, 1);
-}
-.slide-fade-enter, .slide-fade-leave-to
-/* .slide-fade-leave-active 用于 2.1.8 以下版本 */ {
-  transform: translateX(10px);
-  opacity: 0;
-}
-</style>
